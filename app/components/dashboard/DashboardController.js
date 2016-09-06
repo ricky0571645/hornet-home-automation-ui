9234a4a1df7fbddd8b2c048f2a6f25ee3c1a01b1
Ricky Martinez <ricky.0571645@gmail.com>
2016-09-05 19:54:12 -0700

    Reduced Code, Added Icon, Enabled ng-Click to Connect
    Condensed code in order for a more modular design. Commented code for
    better understanding. Using more AngularJS directives.

--- app/components/dashboard/DashboardController.js
+++ app/components/dashboard/DashboardController.js
@@ -2,12 +2,28 @@ app.controller("DashboardController", main);
 
 function main($scope) {
     var vm = this;
-
+    vm.homeItemStack = [];
     vm.connectionLabel = "Disconnected";
     vm.loading = true;
 
-    console.log(vm.loading);
+    //A constructor for the homeItem object
+    function homeItem(itemName, itemTitle, itemData, itemImageSource,isConnected){
+        this.itemName = itemName;
+        this.itemTitle = itemTitle;
+        this.itemData = itemData;
+        this.itemImageSource = itemImageSource;
+        this.isConnected = isConnected;
+    }
 
+    // Add any new cards here. Look at the model above to determine the format. 
+    vm.homeItemStack.push(new homeItem("mainController", "Controller Status", " ", ' ', false));
+    vm.homeItemStack.push(new homeItem("temperature", "Temperature", 87, '/assets/img/weather-icon.png', true));
+    vm.homeItemStack.push(new homeItem("alarm", "Alarm Status", " ", ' ', false));
+    vm.homeItemStack.push(new homeItem("lights", "Light Status", "On", '/assets/img/light_icon.png', true));
+    vm.homeItemStack.push(new homeItem("shutters", "Shutters", "On", '/assets/img/light_icon.png', false));
+    vm.homeItemStack.push(new homeItem("dogWater", "Dog Water", "On", '/assets/img/water.png', true));
+
+    //a manual timeout so that you can see the screen loading
     vm.timeOut =  function () {
        setTimeout(function () 
        {
@@ -18,10 +34,11 @@ function main($scope) {
        }, 3000);
      }
 
+     //attempts to change the vm.connection label to something different (DOESN"T WORK)
      vm.checkingForControllerConnection =  function () {
+        vm.connectionLabel = "Connecting...";
        setTimeout(function () 
        {
-        vm.connectionLabel = "Connecting...";
          $scope.$apply(function()
          {
            vm.connectionLabel = "Disconnected";
--- app/components/login/LoginController.js
+++ app/components/login/LoginController.js
@@ -2,15 +2,18 @@ app.controller("LoginController", main);
 
 function main($scope, $location) {
     var vm = this;
+    // filler variables
     vm.userName = "username";
     vm.password = "password";
     vm.clicked = "Submit";
 
+    // Changes the page to the dashboard page if the data entered is correct
     vm.changePage = function(path)
     {
     		$location.path(path);
     }
 
+    // Changes the page on an enter click if data is correct
     vm.enterKeyChangePage = function(path, keyEvent)
     {
     	if(keyEvent.which === 13)
