27be8889f7edff2f48daf447e207b3bb5f99ead4
Ricky Martinez <ricky.0571645@gmail.com>
2016-09-18 13:21:01 -0700

    Added about/contact page
    Image is clickable and sends email

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,6 +4,12 @@ app.config(['$routeProvider',function($routeProvider) {
 	.when('/dashboard', {
 		templateUrl: 'app/components/dashboard/dashboard.html'
 	})
+	.when('/about', {
+		templateUrl: 'app/components/about/about.html'
+	})
+	.when('/contact', {
+		templateUrl: 'app/components/contact/contact.html'
+	})
 	.otherwise({redirectTo:'/'});
 	
 }]);
